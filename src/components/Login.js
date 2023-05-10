5a0e756cd0c033adc642aedd43f2a4e0d70bec46
Templa-io <humphreyWritter@gmail>
2023-05-10 16:10:39 -0700

    init

---
+++ src/components/Login.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div className='Login'>
+        <div className='Left'>
+            <div className='Header'>
+                <div>Restaurant Management System</div>
+            </div>
+
+            <div className='description'>
+                <div>This pumpkin cream soup </div>
+            <div>will warm up the feintest of hearts </div> 
+             </div>
+            <div className='boxes'>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        <div className='Right'></div>
+    </div>
+  )
+}
+
+export default Login
