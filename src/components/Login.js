6126d257c3b5423c7a9cd5394d970359ea293e3f
Templa-io <humphreyWritter@gmail>
2023-05-10 17:42:47 -0700

    typography

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,18 +5,20 @@ const Login = () => {
   return (
     <div className='Login'>
         <div className='Left'>
+            <div className='vector'>
             <div className='Header'>
                 <div>Restaurant Management System</div>
             </div>
 
             <div className='description'>
-                <div>This pumpkin cream soup </div>
-            <div>will warm up the feintest of hearts </div> 
+                <div>This pumpkin cream soup</div>
+            <div>will warm up the feintest of hearts</div> 
              </div>
             <div className='boxes'>
                 <div></div>
                 <div></div>
                 <div></div>
+            </div>
             </div>
         </div>
         <div className='Right'></div>
