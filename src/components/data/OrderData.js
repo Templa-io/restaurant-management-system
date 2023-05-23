87b9af6430d8e55f6e504d2e44ecdff671aea405
Templa-io <humphreywritter@gmail.com>
2023-05-23 03:30:51 -0700

    update left  hero

---
+++ src/components/data/OrderData.js
@@ -0,0 +1,18 @@
+import image1 from "../../assets/Ellipse 67.png"
+
+export const Orders = [
+{
+    id: 1, name: 'Chicken Shrimp',imageSrc:image1 , price: 'USD 30.00',number:'Order no. 340534', description: '2 Plates and sauce on the sides'
+},
+
+{
+    id: 2, name: 'Chicken Shrimp',imageSrc:image1 , price: 'USD 30.00',number:'Order no. 340534', description: '2 Plates and sauce on the sides'
+},
+{
+    id: 3, name: 'Chicken Shrimp',imageSrc:image1 , price: 'USD 30.00',number:'Order no. 340534', description: '2 Plates and sauce on the sides'
+},
+{
+    id: 4, name: 'Chicken Shrimp',imageSrc:image1 , price: 'USD 30.00',number:'Order no. 340534', description: '2 Plates and sauce on the sides'
+},
+
+]
