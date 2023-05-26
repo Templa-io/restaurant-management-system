561a88526fe3068e0a348993e167ceb39f7448b7
Templa-io <humphreywritter@gmail.com>
2023-05-26 10:47:55 +0300

    menu list page

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -3,20 +3,48 @@ import LeftComponent from './LeftComponent'
 import './MenuList.css'
 import MenuComponent from './subComponents/MenuComponent'
 import CreateComponent from './subComponents/CreateComponent'
+import image1 from '../assets/Rectangle 121.png'
+import { menuData } from './data/MenuData'
 
 const MenuList = () => {
   return (
    
     <div className='Hero'>
-    
     <div className='Hero-left'>
      <LeftComponent/>
-    </div>
+     </div>
+  
+    
+   
     <div className='Hero-right'>
     <MenuComponent/>
     <div className='container'>
-    <div className='active-orders'>
-   
+    <div className='Active-orders'>
+    {menuData && menuData.map(n =>(
+
+            <div>
+      <div className='Dish-section'>
+    <img src={image1} alt=''/>
+    <div className='Span-section'>
+    <div>Octopus with citrus salad & herbs</div>
+    <div>Sem aliquet sit urna aliquam vitae nisl convallis a... </div>
+    <div>USD 30.00</div> 
+    
+    </div>
+    </div>
+      <div className='Dish-section1'>
+    <img src={image1} alt=''/>
+    <div className='Span-section'>
+    <div>Octopus with citrus salad & herbs</div>
+    <div>Sem aliquet sit urna aliquam vitae nisl convallis a... </div>
+    <div>USD 30.00</div> 
+    
+    </div>
+    </div>
+    </div>
+    ))}
+
+  
     </div>
     <CreateComponent/>
  
---
+++ src/components/data/MenuData.js
@@ -0,0 +1,14 @@
+import image1 from "../../assets/Ellipse 67.png"
+
+export const menuData = [
+{
+    id: 1, name: 'Octopus with citrus salad & herbs',imageSrc:image1 , price: 'USD 30.00', description: 'Sem aliquet sit urna aliquam vitae nisl convallis a...'
+},
+
+{
+    id: 2, name: 'Octopus with citrus salad & herbs',imageSrc:image1 , price: 'USD 30.00', description: 'Sem aliquet sit urna aliquam vitae nisl convallis a...'
+},
+{
+    id: 3, name: 'Octopus with citrus salad & herbs',imageSrc:image1 , price: 'USD 30.00', description: 'Sem aliquet sit urna aliquam vitae nisl convallis a...'
+},
+]
