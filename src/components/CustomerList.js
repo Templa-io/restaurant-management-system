d4e5dce4b313adf22ff76b3d6d12f2baba246412
Templa-io <humphreywritter@gmail.com>
2023-05-26 19:01:29 +0300

    Customer list page

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,9 +1,51 @@
 import React from 'react'
 import './CustomerList.css'
+import LeftComponent from './LeftComponent'
+import MenuComponent from './subComponents/MenuComponent'
+import { custData } from './data/CustomerData'
 
 const CustomerList = () => {
   return (
-    <div>CustomerList</div>
+    <div className='Hero'>
+    
+    <div className='Hero-left'>
+     <LeftComponent/>
+    </div>
+    <div className='Hero-right'>
+    <MenuComponent/>
+    <div className='container'>
+    <div className='Active-orders'>
+    {custData && custData.map(n =>(
+<div className='email-order'>
+  <div>
+  <div className='ppic'></div>
+  <div className='names'>
+  <div>{n.name}</div>
+  <div>{n.email}</div>
+  </div>
+  </div>
+  
+  <div className='Detail-btn'>{n.button}</div>
+  </div>
+    ))}
+  
+    </div>
+    <div className='Order-details'>
+    <div>Customer Profile</div>
+  <div className='customer-profile'> 
+  <div></div>
+  <div className='profile-details'>
+<div>John Doe</div>
+  <div>johndoe@gmail.com</div>
+  <div>120 Orders</div>
+  <div> View all orders</div>
+  </div>
+  </div>
+</div>
+    </div>
+    </div>
+    
+    </div>
   )
 }
 
---
+++ src/components/data/CustomerData.js
@@ -0,0 +1,20 @@
+export const custData = [
+    {
+        id: 1, name: 'John Doe', email: 'johndoea@gmail.com',button:'Details',
+    },
+    
+    {
+        id: 2, name: 'John Doe', email: 'johndoea@gmail.com',button:'Details',
+    },
+    {
+        id: 3, name: 'John Doe', email: 'johndoea@gmail.com',button:'Details',
+    },
+    {
+        id: 4, name: 'John Doe', email: 'johndoea@gmail.com',button:'Details',
+    },
+    {
+        id: 5, name: 'John Doe', email: 'johndoea@gmail.com',button:'Details',
+    },
+    
+    
+    ]
