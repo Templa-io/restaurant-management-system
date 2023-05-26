4b6ef60d0c7b49468ee517bae153130056318fa9
Templa-io <humphreywritter@gmail.com>
2023-05-27 00:56:30 +0300

    update navigation

--- src/components/EmptyOrderList.js
+++ src/components/EmptyOrderList.js
@@ -8,47 +8,14 @@ import image5 from '../assets/Ellipse 68.png'
 import image6 from '../assets/Ellipse 69.png'
 import image7 from '../assets/Image (2).png'
 import { Link } from 'react-router-dom'
+import LeftComponent from './LeftComponent'
 
 
 const EmptyOrderList = () => {
   return (
     <div className='Hero'>
-       <div className='Hero-left'>
-        <div>
-            <span>Restaurant system</span>
-        </div>
-        <div className='orders'>
-            <div>
-                 <img src={image1} alt='' />
-            <div> Orders</div>
-            <div className='num'>0</div>
-
-            </div>
-        <div>
-            <img src={image2} alt=''/>
-            <div>Menus</div>
-        </div>
-        <div>
-            <img src={image3} alt='' />
-            <div>Customers</div>
-        </div>
-        </div>
-        <div className='menu'>
-            <div className='img'>
-<img src={image4} alt='' />
-<img src={image5} alt=''/>
-<img src={image6} alt=''/>
-            </div>
-            <div>Organize your menu through the button below 
-            
-            <Link to="/emptyMenu">
-             <div className='menu-button'>+Add menu</div>
-            </Link>     
-           
-            </div>
-        </div>
-       
-       </div> 
+    <div className='Hero-left'>
+    <LeftComponent/></div>
        <div className='Hero-right'>
         <div className='admin'>
             <div><span>My orders.</span></div>
